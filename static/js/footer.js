bd38d78a71334661f056b9c8a7131e0e61daea08
tkddn0518 <tkddn0518@naver.com>
2021-07-20 23:55:03 +0900

    home.html, home.css - second section

---
+++ static/js/footer.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    document.getElementById("footer").innerHTML='<object type="text/html" data="footer.html"></object>';
+})
+
+
+// 출처: https://soft91.tistory.com/27 [너와 나의 프로그래밍]
